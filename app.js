0c0fd67b4894987b4abdf0da5023a2ee3f8d0f3f
Jason Underhill <jason.underhill@bookwisesolutions.com>
2018-01-11 17:29:27 +0000

    response data showing as undefined

--- app.js
+++ app.js
@@ -21,17 +21,19 @@ var handlers = {
     },
 
     "CurrentPrice": function(){
-        var speechOutput = "";
-        var prices = data.getCurrentPrice();
-        speechOutput += "Bitcoin is currently " + prices.usd + " U.S dollars or " + prices.gbp + " British pounds.";
-
-        this.emit(':tell', speechOutput);
+        var callback = function(prices){
+            var speechOutput = "";
+            speechOutput += "Bitcoin is currently " + prices.usd + " U.S dollars or " + prices.gbp + " British pounds.";
+            this.emit(':tell', speechOutput);
+        };
+        var prices = data.getCurrentPrice(callback);
     }
 };
 
 exports.handler = function(event, context){
     var alexa = Alexa.handler(event, context);
     alexa.appId = "amzn1.ask.skill.e4ee2508-f2b3-400b-96bc-1deb6ccce518";
+    alexa.appId = 'amzn1.echo-sdk-ams.app.000000-d0ed-0000-ad00-000000d00ebe';
     alexa.registerHandlers(handlers);
     alexa.execute();
 }
--- data.js
+++ data.js
@@ -2,7 +2,8 @@ var https = require('https');
 
 const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
-exports.getCurrentPrice = function(){
+exports.getCurrentPrice = function(callback){
+    console.log("getCurrentPrice()");
     var currentPrice = "";
     var prices = {};
     https.get(url, (response) => {
@@ -11,12 +12,17 @@ exports.getCurrentPrice = function(){
             data += chunk;
         });
         response.on('end', () => {
+            console.log("Data: " + data);
             var parsed = JSON.parse(data);
-
+            console.log("Parsed: " + parsed);
+            console.log(data);
+            console.log(data['bpi']['USD']);
+            console.log(data.chartName);
+            console.log(data.bpi.USD);
             prices.usd = data.bpi.USD.rate;
             prices.gbp = data.bpi.GBP.rate;
 
-            return prices;
+            callback(prices);
         });
     })
     .on('error', (err) => {
